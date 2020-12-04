0ef8bc3e4e33f1bed1e11eb8fede82c5563b6e11
ilyas-code <ilyas515253@gmail.com>
2020-12-04 14:13:15 +0530

    build input fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-
-
+  const [blogPost, setBlogPost] = useState(null)
+  const [postTime, setPostTime] = useState(null)
+  const time = new Date();
+  
+  //function for setting the blog text in blog hook
+  function changeHandler(e) {
+    const value = e.target.value;
+    setBlogPost(value);
+    console.log(blogPost);
+  }
+  
+  // function for submitting the data to the database
+  function submitHandle(e) {
+    e.preventDefault();
+    setPostTime(time.toDateString());
+    console.log(postTime);
+  }
 
   return (
     <div className="App">
-      <nav class="navbar navbar-light bg-primary ">
-        <span class="navbar-brand mb-0 h1 text-white">Blog-Site</span>
+      <nav className="navbar navbar-light bg-primary ">
+        <span className="navbar-brand mb-0 h1 text-white">Blog-Site</span>
       </nav>
       <form>
         <div className="card mt-5" style={{ maxWidth: "500px", margin: "0 auto" }}>
           <h1 className="card-header">Blog</h1>
-          <div className="card-body"></div>
-          <p class="card-text">Share your Thoughts</p>
-          <textarea className=" form-control" rows="3" placeholder="Do Something Here..."></textarea>
-          <button type="submit" className="btn btn-primary w-25 m-3"><i class="fa fa-paper-plane" aria-hidden="true">  Post</i></button>
+          <div className="card-body">
+            <p className="card-text">Share your Thoughts</p>
+            <textarea name="blogText" className=" form-control" rows="3" placeholder="Do Something Here..." onChange={changeHandler}></textarea>
+            <button type="submit" className="btn btn-primary w-25 mt-3 float-left" onClick={submitHandle}>
+              <i className="fa fa-paper-plane" aria-hidden="true"> Post</i>
+            </button>
+          </div>
+
         </div>
       </form>
-
+      {/* blog post card */}
+      <div className="card mt-5" style={{ maxWidth: "500px", margin: "0 auto" }}>
+        <div className="bg-light border-bottom">
+          <p className="float-left m-1">{time.toDateString()}</p>
+        </div>
+        <p className="card-text">hi</p>
+      </div>
     </div>
   );
 }
