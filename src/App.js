301b9e985c271c193729cfc9d29884bd0c0697a4
ilyas-code <ilyas515253@gmail.com>
2020-12-17 20:11:58 +0530

    connected to backend mongodb server and working well

--- src/App.js
+++ src/App.js
@@ -1,59 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import BlogPlate from "./components/BlogPlate";
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [blogPost, setBlogPost] = useState(null)
+  const [blogPost, setBlogPost] = useState({
+    userName: "mohammed",
+    BlogText: "ex",
+  });
   const [apiData, setApiData] = useState({
     _id: "",
-    userName: '',
+    userName: "",
     BlogText: [
-      {id:0, Text: 'loading', date: "2020-12-08T10:57:43.578Z"},
-      { id:1,Text: 'loading', date: "2020-12-08T10:57:43.578Z"},
-      { id:2,Text: 'loading', date: "2020-12-08T10:57:43.578Z"}
+      { id: 0, Text: "loading", date: 0 },
       
     ]
-  })
+  });
 
+  // calling Blog data from the Api
   useEffect(() => {
     var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+      method: "GET",
+      redirect: "follow",
     };
-
+    // fetching data of the user modammed
     fetch("http://localhost:8000/getBlog/mohammed", requestOptions)
-      .then(response => response.json())
-      .then(result =>  setApiData(result))
-      .catch(error => console.log('error', error));
-
-
-  }, [])
+      .then((response) => response.json())
+      .then((result) => setApiData(result))
+      .catch((error) => console.log("error", error));
+  }, []);
 
   // const time = new Date();
   // const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  console.log(apiData.BlogText);
+
   //function for setting the blog text in blog hook
   function changeHandler(e) {
-
     const value = e.target.value;
     setBlogPost({
       userName: "mohammed",
-      BlogText: value
+      BlogText: value,
     });
-    console.log(blogPost);
-
   }
 
   // function for submitting the data to the database
   function submitHandle(e) {
+    var data = JSON.stringify(blogPost);
 
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        alert(this.responseText);
+      }
+    });
+
+    xhr.open("POST", "http://localhost:8000/postBlog");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+
+    
+
+    console.log("ticks");
   }
 
-  const blogText = apiData.BlogText
+  const blogText = apiData.BlogText.sort(function (a, b) {
+    return new Date(b.date) - new Date(a.date);
+  });
   const blogArray = blogText.map((ele) => {
-    return <BlogPlate key ={ele.id} time={ele.date} txt={ele.Text} />
-  })
+    return <BlogPlate key={ele.date} date={ele.date} Text={ele.Text} blogPost={blogPost} />;
+  });
 
   // const blogArray = array.map((ele) => {
   //   return <BlogPlate key={ele} time={time} txt={ele} />
@@ -61,28 +78,40 @@ function App() {
 
   return (
     <div className="App">
-
       <NavBar />
 
       <form>
-        <div className="card mt-5" style={{ maxWidth: "500px", margin: "0 auto" }}>
+        <div
+          className="card mt-5"
+          style={{ maxWidth: "500px", margin: "0 auto" }}
+        >
           <h1 className="card-header">Blog</h1>
           <div className="card-body">
             <p className="card-text">Share your Thoughts</p>
-            <textarea name="blogText" className=" form-control" rows="3" placeholder="Do Something Here..." onChange={changeHandler}></textarea>
-            <button type="submit" className="btn btn-primary w-25 mt-3 float-left" onClick={submitHandle}>
-              <i className="fa fa-paper-plane" aria-hidden="true"> Post</i>
+            <textarea
+              name="blogText"
+              style={{ resize: "none" }}
+              className="form-control"
+              rows="3"
+              placeholder="Do Something Here..."
+              onChange={changeHandler}
+            ></textarea>
+
+            <button
+              type="submit"
+              className="btn btn-primary w-25 mt-3 float-left"
+              onClick={submitHandle}
+            >
+              <i className="fa fa-paper-plane" aria-hidden="true">
+                {" "}
+                Post
+              </i>
             </button>
           </div>
-
         </div>
       </form>
 
-      <div className="flex-column">
-        {blogArray}
-      </div>
-
-
+      <div className="flex-column">{blogArray}</div>
     </div>
   );
 }
--- src/components/BlogPlate.js
+++ src/components/BlogPlate.js
@@ -1,17 +1,40 @@
 import React from 'react'
 
 function BlogPlate(props) {
-    const dateString = new Date(props.time);
+    const { Text, date, blogPost } = props;
+    const dateString = new Date(date);
+    //delete Blog Query
+    const deleteQuery = { userName: blogPost.userName, BlogText: { Text } };
+
+    function DeleteHandle(e) {
+        
+        var data = JSON.stringify(deleteQuery);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                alert(this.responseText);
+            }
+        });
+
+        xhr.open("DELETE", "http://localhost:8000/deleteBlog");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    }
+
     return (
         <div className="card mt-5" style={{ maxWidth: "500px", margin: "0 auto" }}>
             <div className="bg-light border-bottom flex-row">
                 <p className="float-left m-1">{dateString.toDateString()}</p>
 
-                <button type="submit" className="btn btn-danger pl-3 pr-3 p-1 float-right" >
+                <button type="submit" className="btn btn-danger pl-3 pr-3 p-1 float-right" onClick={DeleteHandle}>
                     <i className="fa fa-trash-o" aria-hidden="true"></i>
                 </button>
             </div>
-            <p className="card-text text-left p-2">{props.txt}</p>
+            <p className="card-text text-left p-2">{Text}</p>
         </div>
     )
 }
