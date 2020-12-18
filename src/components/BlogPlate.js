fa767051ffd829c3959d817e116e4b836137cc30
ilyas-code <ilyas515253@gmail.com>
2020-12-18 17:50:16 +0530

    fixed rendering issues by migrating the components

--- src/components/BlogPlate.js
+++ src/components/BlogPlate.js
@@ -1,28 +1,15 @@
 import React from 'react'
 
 function BlogPlate(props) {
-    const { Text, date, blogPost } = props;
+    const { Text, date, blogPost, DeleteHandle } = props;
     const dateString = new Date(date);
     //delete Blog Query
     const deleteQuery = { userName: blogPost.userName, BlogText: { Text } };
 
-    function DeleteHandle(e) {
-        
-        var data = JSON.stringify(deleteQuery);
 
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+    function RemoveBlog(e) {
+        DeleteHandle(e, deleteQuery)
 
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                alert(this.responseText);
-            }
-        });
-
-        xhr.open("DELETE", "http://localhost:8000/deleteBlog");
-        xhr.setRequestHeader("Content-Type", "application/json");
-
-        xhr.send(data);
     }
 
     return (
@@ -30,8 +17,8 @@ function BlogPlate(props) {
             <div className="bg-light border-bottom flex-row">
                 <p className="float-left m-1">{dateString.toDateString()}</p>
 
-                <button type="submit" className="btn btn-danger pl-3 pr-3 p-1 float-right" onClick={DeleteHandle}>
-                    <i className="fa fa-trash-o" aria-hidden="true"></i>
+                <button type="submit" className="btn  pl-3 pr-3 p-1 float-right" onClick={RemoveBlog}>
+                    <i id="del-icon" className="fa fa-trash" aria-hidden="true"></i>
                 </button>
             </div>
             <p className="card-text text-left p-2">{Text}</p>
---
+++ src/components/Main.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import BlogPlate from "./BlogPlate";
+
+function Main() {
+    // Setting Blog data for posting to server
+    const [blogPost, setBlogPost] = useState({
+        userName: "mohammed",
+        BlogText: "ex",
+    });
+
+    // Setting the Api data fetchecd from the server 
+    const [apiData, setApiData] = useState({
+        _id: "",
+        userName: "",
+        BlogText: [
+            { id: 0, Text: "loading", date: 0 },
+
+        ]
+    });
+
+
+    // Refreshing the component after calling Blog data from the Api
+    useEffect(() => {
+
+        GetData();
+
+    }, []);
+
+
+
+    // Function for fetching the blog data of the user from the server by GET request
+    async function GetData() {
+        var requestOptions = {
+            method: "GET",
+            redirect: "follow",
+        };
+        // fetching data of the user modammed
+        try {
+            const response = await fetch("http://localhost:8000/getBlog/mohammed", requestOptions)
+            const result = await response.json();
+            setApiData(result);
+            console.log(result);
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    //function for setting the blog text in blog hook
+    function ChangeHandler(e) {
+        const value = e.target.value;
+        setBlogPost({
+            userName: "mohammed",
+            BlogText: value,
+        });
+        console.log(blogPost);
+    }
+
+    // function for submitting the data to the database
+    async function SubmitHandle(e) {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify(blogPost);
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        try {
+            const response = await fetch("http://localhost:8000/postBlog", requestOptions)
+            const result = await response.text();
+            console.log(result);
+        } catch (error) {
+            console.log("error occured", error);
+        }
+
+
+
+
+    }
+    async function DeleteHandle(e, deleteQuery) {
+        var data = JSON.stringify(deleteQuery);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var requestOptions = {
+            method: 'DELETE',
+            headers: myHeaders,
+            body: data,
+            redirect: 'follow'
+        };
+        try {
+            const response = await fetch("http://localhost:8000/deleteBlog", requestOptions)
+            const result = await response.text();
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+
+        GetData();
+    }
+
+    // Sorting the data fetched from the server for maping on the component
+    const blogText = apiData.BlogText.sort(function (a, b) {
+        return new Date(b.date) - new Date(a.date);
+    });
+
+    // Mapping the fetched data to the resuable component
+    const blogArray = blogText.map((ele) => {
+        return <BlogPlate
+            key={ele.date}
+            date={ele.date}
+            Text={ele.Text}
+            blogPost={blogPost}
+            GetData={GetData}
+            DeleteHandle={DeleteHandle}
+        />;
+    });
+
+
+    return (
+        <div>
+            <form>
+                <div
+                    className="card mt-5"
+                    style={{ maxWidth: "500px", margin: "0 auto" }}
+                >
+                    <h1 className="card-header">Blog</h1>
+                    <div className="card-body">
+                        <p className="card-text">Share your Thoughts</p>
+                        <textarea
+                            name="blogText"
+                            style={{ resize: "none" }}
+                            className="form-control"
+                            rows="3"
+                            placeholder="Do Something Here..."
+                            onChange={ChangeHandler}
+                        ></textarea>
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary w-25 mt-3 float-left"
+                            onClick={SubmitHandle}
+                        >
+                            <i id="Post-ico" className="fa fa-paper-plane" aria-hidden="true">
+                                <p>Post</p>
+                            </i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <div className="flex-column">
+                {blogArray}
+            </div>
+        </div>
+    )
+}
+
+export default Main
